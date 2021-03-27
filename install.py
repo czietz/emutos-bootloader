d6070346de1412e02a784bb8dc69d82c3bc84a36
czietz <czietz@07ef0cb5-9c8c-4f91-99f2-81ebaa534cd8>
2021-03-27 15:51:15 +0000

    Rework Python installer to cover all (plausible) scenarios
    This is in preparation of an SD card image creation tool.
    
    
    
    git-svn-id: https://www.chzsoft.de/public/subversion/bootloader@22 07ef0cb5-9c8c-4f91-99f2-81ebaa534cd8

--- install.py
+++ install.py
@@ -18,7 +18,46 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE. 
+# IN THE SOFTWARE.
+
+
+# This installer needs to consider three scenarios:
+
+# 1. ACSI / SCSI device or IDE device on an interface with Smartswap:
+#    In these cases, there is no difference in byte order between byte order
+#    on the Atari and the PC, i.e., there is just a single byte order. Thus:
+#    - Partition table: single byte order.
+#    - Root sector code: single byte order.
+#    - Boot sector data and code: single byte order.
+
+# 2. IDE device (without Smartswap), PC-compatible partitioning:
+#    In this case, the partition table and boot sector are in PC byte order,
+#    otherwise, they would not be readable. The root sector code, however,
+#    needs to be in Atari byte order (=swapped), to be executable. Thus:
+#    - Partition table: PC byte order.
+#    - Root sector code: Atari byte order (=swapped from PC perspective).
+#    - Boot sector data and code: PC byte order.
+
+# 3. IDE device (without Smartswap), Atari-compatible partitioning:
+#    In this case, everything is in Atari byte order, therefore unreadable
+#    on the PC.
+#    - Partition table: Atari byte order (=swapped from PC perspective).
+#    - Root sector code: Atari byte order (=swapped from PC perspective).
+#    - Boot sector data and code: Atari byte order (=swapped from PC perspective).
+
+# Further complication I: Scenario 3 can be auto-detected by checking the
+# partition table. However, scenarios 1 and 2 cannot be distinguished from each
+# other. Users need to tell the installer if they intend to use the disk image
+# as IDE device (without Smartswap).
+
+# Further complication II: Scenarios 1 and 3 could use either an PC-style or an
+# Atari-style partition table. In contrast, in scenario 2 -- which is intended
+# for PC compatibility -- an Atari-style partition table does not make sense.
+
+# Further complication III: In scenario 3 (where the entire file system is
+# byte-swapped from the PC's perspective), 'mcopy' cannot install EMUTOS.SYS
+# into the image. Users will have to copy it themselves.
+
 
 import sys
 import struct
@@ -29,68 +68,108 @@ ROOT_FILE = "root.bin"
 BOOT_FILE = "bootsect.bin"
 
 if len(sys.argv)<2:
-    sys.exit("Usage: %s <hd-image> [emutos.prg]" % sys.argv[0])
+    sys.exit("Usage: %s [-ideswap] <hd-image> [emutos.prg]" % sys.argv[0])
 
+# Scenario 2 (see above): IDE with PC-compatible partitioning
+if sys.argv[1] == "-ideswap":
+    ideswap = True
+    del sys.argv[1] # skip this argument
+else:
+    ideswap = False
 
 with open(sys.argv[1], "r+b") as f:
 
-    # INSTALL ROOT SECTOR at offset 0
-    with open(ROOT_FILE, "rb") as c:
-        code = c.read(442)
-        f.write(code)
-
-    # FIND FIRST PARTITION
+    # FIND FIRST PARTITION AND DETERMINE SCENARIO
     f.seek(0)
     mbr = f.read(512)
-    magic = struct.unpack("<H", mbr[510:])[0] # Intel byte order
-    if magic == 0xAA55:
+    # PC MBR magic
+    pcmagic = struct.unpack("<H", mbr[510:])[0] # Intel byte order
+    # Atari partition IDs for non-byte-swapped and byte-swapped scenarios
+    atarimagic  = mbr[0x1C7:0x1CA]
+    atarimagic2 = mbr[0x1C6:0x1C7] + mbr[0x1C9:0x1CA] + mbr[0x1C8:0x1C9] 
+    if pcmagic == 0xAA55:
         print("Detected DOS partition table (non-byteswapped)")
         lba = struct.unpack("<L", mbr[0x1BE+8:0x1BE+12])[0]
-        swap = False
         print("First partition at LBA = %d" % lba)
         if lba == 0:
             sys.error("Illegal partition table")
         # mark as bootable
         f.seek(0x1BE)
         f.write(b'\x80')
-    elif magic == 0x55AA:
+        # could be either scenario 1 or 2, let user decide
+        if ideswap:
+            scenario = 2
+        else:
+            scenario = 1
+    elif pcmagic == 0x55AA:
         print("Detected DOS partition table (byteswapped)")
         lba = struct.unpack(">HH", mbr[0x1BE+8:0x1BE+12])
         lba = lba[1]*256 + lba[0]
-        swap = True
         print("First partition at LBA = %d" % lba)
         if lba == 0:
             sys.error("Illegal partition table")
         # mark as bootable
         f.seek(0x1BF)
         f.write(b'\x80')
-    else:
-        print("Assuming Atari partition table")
+        # this is scenario 3
+        scenario = 3
+    elif atarimagic == b"BGM" or atarimagic == b"GEM":
+        print("Detected Atari partition table (non-byteswapped)")
         lba = struct.unpack(">L", mbr[0x1C6+4:0x1C6+8])[0]
-        swap = False
         print("First partition at LBA = %d" % lba)
         if (lba == 0) or ((mbr[0x1C6] & 0x7f != 1)): # check partition flag
             sys.error("Illegal partition table")
         # mark as bootable
         f.seek(0x1C6)
         f.write(b'\x81')
+        # scenario 2 is unplausible with an Atari partition table
+        scenario = 1
+    elif atarimagic2 == b"BGM" or atarimagic2 == b"GEM":
+        print("Detected Atari partition table (byteswapped)")
+        lba = struct.unpack("<HH", mbr[0x1C6+4:0x1C6+8])
+        lba = lba[0]*256 + lba[1]
+        print("First partition at LBA = %d" % lba)
+        if (lba == 0) or ((mbr[0x1C7] & 0x7f != 1)): # check partition flag
+            sys.error("Illegal partition table")
+        # mark as bootable
+        f.seek(0x1C7)
+        f.write(b'\x81')
+        # this is scenario 3
+        scenario = 3
+    else:
+        sys.exit("ERROR: No supported partition detected!")
+
+    print("DEBUG: scenario %d" % scenario)
+
+    # INSTALL ROOT SECTOR at offset 0
+    f.seek(0)
+    with open(ROOT_FILE, "rb") as c:
+        code = bytearray(c.read(442))
+        # swap code for IDE devices
+        if scenario >= 2:
+            code[0::2],code[1::2] = code[1::2],code[0::2]
+        f.write(code)
 
     # MAKE ROOT SECTOR BOOTABLE (by correcting checksum)
+    if scenario >= 2:
+        endian = "<"
+    else:
+        endian = ">"
     f.seek(442)
-    f.write(struct.pack(">H", 0))
+    f.write(struct.pack(endian+"H", 0))
     f.seek(0)
     mbr = f.read(512)
     sum = 0
-    for word in struct.iter_unpack(">H", mbr): # Motorola byte order
+    for word in struct.iter_unpack(endian+"H", mbr): # Motorola byte order
         sum += word[0]
     sum = 0x1234 - sum
     sum = sum % 65536 # always positive in Python
     f.seek(442)
-    f.write(struct.pack(">H", sum))
+    f.write(struct.pack(endian+"H", sum))
     
     # INSTALL BOOT SECTOR
     offset = lba * 512 # Offset of boot sector
-    if swap:
+    if scenario == 3:
         endian = "<"
     else:
         endian = ">"
@@ -102,8 +181,7 @@ with open(sys.argv[1], "r+b") as f:
         f.seek(offset + 0x3e)
         code = bytearray(c.read(446))
         # byte-swap code if required
-        if swap:
-            #code = struct.pack("<%dH"%(len(code)/2), *struct.unpack(">%dH"%(len(code)/2), code))
+        if scenario == 3:
             code[0::2],code[1::2] = code[1::2],code[0::2]
         f.write(code)
     
@@ -122,8 +200,8 @@ with open(sys.argv[1], "r+b") as f:
     f.write(struct.pack(endian+"H", sum))
 
 if len(sys.argv) >= 3:
-    if swap:
-        sys.exit("Copying EmuTOS.PRG is only supported on non-byteswapped images")
+    if scenario == 3:
+        sys.exit("Copying EmuTOS.PRG is only supported on images where the filesystem is in PC byte order")
     print("Copying '%s'" %  sys.argv[2])
     try:
         # Use Mtools mcopy: -o to overwrite existing file, -i to specify image file and offset, mtools_skip_check to relax FAT checks
